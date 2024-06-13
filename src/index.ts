ff5bb4b3e481d67801af39eeba2023b0cc76f124
Fabio de Moura Martins <fabio.mmouras@hotmail.com>
2024-06-13 12:59:48 -0300

    feat: add express initial server

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,14 @@
-console.log('helloo');
+import express from 'express';
+import { config } from 'dotenv';
+
+config();
+
+const app = express();
+
+const port = process.env.PORT || 8000;
+
+app.get('/', (req, res)=>{
+    res.send('hello world')
+})
+
+app.listen(port, () => console.log(`Listening on port ${port}!`));
