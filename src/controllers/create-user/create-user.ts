8c409b26a65b32705794e9fd669e2269a4951dde
Fabio de Moura Martins <fabio.mmouras@hotmail.com>
2024-06-13 23:23:19 -0300

    refactor: add IController common interface

--- src/controllers/create-user/create-user.ts
+++ src/controllers/create-user/create-user.ts
@@ -1,13 +1,9 @@
 import validator from 'validator';
 import { User } from '../../models/user';
-import { HttpRequest, HttpResponse } from '../protocols';
-import {
-    CreateUserParams,
-    ICreateUserController,
-    ICreateUsersRepository
-} from './protocols';
+import { HttpRequest, HttpResponse, IController } from '../protocols';
+import { CreateUserParams, ICreateUsersRepository } from './protocols';
 
-export class CreateUserController implements ICreateUserController {
+export class CreateUserController implements IController {
     constructor(
         private readonly createUserRepository: ICreateUsersRepository
     ) {}
--- src/controllers/update-user/update-user.ts
+++ src/controllers/update-user/update-user.ts
@@ -1,10 +1,10 @@
 import { User } from '../../models/user';
-import { HttpRequest, HttpResponse } from '../protocols';
-import { IUpdateUserController, IUpdateUserRepository } from './protocols';
+import { HttpRequest, HttpResponse, IController } from '../protocols';
+import { IUpdateUserRepository, UpdateUserParams } from './protocols';
 
-export class UpdateUserController implements IUpdateUserController {
+export class UpdateUserController implements IController {
     constructor(private readonly updateUserRepository: IUpdateUserRepository) {}
-    async handle(httpRequest: HttpRequest<any>): Promise<HttpResponse<User>> {
+    async handle(httpRequest: HttpRequest<UpdateUserParams>): Promise<HttpResponse<User>> {
         try {
             const id = httpRequest.params.id;
             const body = httpRequest.body;
@@ -15,6 +15,14 @@ export class UpdateUserController implements IUpdateUserController {
                     body: 'Missing user id'
                 };
             }
+ 
+            if (!body) {
+                return {
+                    statusCode: 400,
+                    body: 'Missing fields'
+                };
+            }
+
             const allowedFieldsToUpdate = ['firstName', 'lastName', 'password'];
 
             const someFieldIsNotAllowedToUpdate = Object.keys(body).some(
